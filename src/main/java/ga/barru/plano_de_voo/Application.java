4ed5abdf28b5ef7156d1451bce06efb4e18fcc19
luiz.mauricio <luizbarruga@gmail.com>
2024-08-25 13:57:45 -0300

    Versioned Spring initializer generate project

---
+++ src/main/java/ga/barru/plano_de_voo/Application.java
@@ -0,0 +1,13 @@
+package ga.barru.plano_de_voo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/ga/barru/plano_de_voo/PlanoDeVooApplicationTests.java
@@ -0,0 +1,13 @@
+package ga.barru.plano_de_voo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanoDeVooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
